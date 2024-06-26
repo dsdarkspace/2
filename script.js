ec1fcbc30db1a1740bc6c30ba48cba507419488b
Basel Jawash <147449492+dsdarkspace@users.noreply.github.com>
2024-06-27 02:45:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('HEN1').addEventListener('click', function() {
+  window.location.href = '\HEN1\index.html';
+});
+
+document.getElementById('HEN2').addEventListener('click', function() {
+  window.location.href = '\HEN2\index.html';
+});
+
+
+document.getElementById('HEN3').addEventListener('click', function() {
+  window.location.href = '\HEN3\index.html';
+});
